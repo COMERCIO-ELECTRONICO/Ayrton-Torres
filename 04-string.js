ae89de81cdc70513110b184ba0b999017fe761fb
Ayrton-Torres <59022587+Ayrton-Torres@users.noreply.github.com>
2020-05-13 15:05:29 -0500

    Arreglos-Strings-Eventos

---
+++ 04-string.js
@@ -0,0 +1,32 @@
+var nombre = 'Ayrton'
+
+//console.log(nombre.length); --->posicion de la letra en la que se enecuentra
+
+//console.log(nombre.indexOf('o'));
+
+//console.log(nombre.search('o')); ---> hace lo mismo que indes of
+
+//console.log(nombre.slice('1,4')); --->dividir la palabra pero solo la del final
+
+//console.log(nombre.substr(1,4)); ---> divide la palabra desde la posicion 1 y la ultima
+
+//console.log(nombre.toLowerCase(1,4));
+
+//console.log(nombre.toUpperCase(1,4));
+
+var texto ='hola mundo'
+console.log(texto.replace('hola', 'hello')); 
+
+console.log(texto.concat(', ', nombre));  //concatenar la palabra
+
+console.log(`${texto} mas mi nombre: ${nombre}`);
+
+console.log(texto.trim());
+
+console.log(texto.split(''));
+
+console.log(texto.charAt(1)); //---> retonarme la letra de acuerdo a la posicion que se encuentre
+
+
+
+
