3c74ab6397640afc75c8c411a511e3c64fef62c2
Ayrton <ayrton_97_jairo@hotmail.com>
2020-05-14 18:06:35 -0500

    Json,Outputs,Inputs,Binding

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'front-end';
+
+  tituloPadre = 'Soy tu Padre';
+  nombreBotonPadre = 'alertar!!!!!';
+  imagenPadre = 'https://images7.memedroid.com/images/UPLOADED128/56fac5463ee89.jpeg';
+
+  datosHijo = {
+  tituloPadre : 'Soy tu Padre bro',
+  nombreBotonPadre : 'alertar!!!!!',
+  imagenPadre : 'https://images7.memedroid.com/images/UPLOADED128/56fac5463ee89.jpeg'
+  };
+
+  obtenerRespuestaHijo(evento){
+    console.log(evento)
+  }
 }
