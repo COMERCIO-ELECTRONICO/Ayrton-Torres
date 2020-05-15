00f9226b74320274b8764e0d598632d12f392cc1
Ayrton <ayrton_97_jairo@hotmail.com>
2020-05-15 15:15:53 -0500

    ng model

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MiPrimerComponenteComponent } from './mi-primer-componente/mi-primer-componente.component';
 import { PadreComponent } from './padre/padre.component';
 import { HijoComponent } from './hijo/hijo.component';
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -16,7 +16,7 @@ import { HijoComponent } from './hijo/hijo.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule // modulo de formularios
   ],
   providers: [],
   bootstrap: [AppComponent]
--- front-end/src/app/padre/padre.component.ts
+++ front-end/src/app/padre/padre.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class PadreComponent implements OnInit {
 
   constructor() { }
-
+  valorDelImput = '';
   ngOnInit(): void {
   }
 
